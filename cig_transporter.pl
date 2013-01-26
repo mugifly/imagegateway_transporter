56049ad6faa06217a2a31fd7bd48fe39ec80cb95
Masanori Ohgita <piyo3728@gmail.com>
2013-01-26 23:46:43 +0900

    Renamed and fixed miss.

--- cig_transporter.pl
+++ cig_transporter.pl
@@ -1,4 +1,6 @@
 #! /usr/bin/env perl
+# iMAGE GATEWAY Transporter - 1.0.0
+# https://github.com/mugifly/imagegateway_transporter
 
 use strict;
 use warnings;
@@ -6,20 +8,22 @@ use utf8;
 use 5.10.0;
 
 use lib 'lib/';
-use CIG::MailParser;
-use CIG::ImageFetcher;
+use ImageGatewayNet::MailParser;
+use ImageGatewayNet::ImageFetcher;
 
-my $parser = CIG::MailParser->new();
-my $fetcher = CIG::ImageFetcher->new();
+my $parser = ImageGatewayNet::MailParser->new();
+my $fetcher = ImageGatewayNet::ImageFetcher->new();
 
 # Parse a mail
-my $page_url = $parser->parse($mail);
+my $mail_text = <<'EOF';
 
+EOF
+
+my $page_url = $parser->parse($mail_text);
 if (defined($page_url)) {
 	say $page_url;
 	# Fetch a image
 	$fetcher->fetch_save($page_url, 'image/');
 }
-
 
 exit;
